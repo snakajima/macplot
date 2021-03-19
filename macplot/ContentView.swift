f126a27b2cd8999d4a8dc22bec3a78d214acef28
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2021-03-19 14:32:42 -0700

    xcode project

---
+++ macplot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  macplot
+//
+//  Created by SATOSHI NAKAJIMA on 3/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ macplot/macplotApp.swift
@@ -0,0 +1,17 @@
+//
+//  macplotApp.swift
+//  macplot
+//
+//  Created by SATOSHI NAKAJIMA on 3/19/21.
+//
+
+import SwiftUI
+
+@main
+struct macplotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
