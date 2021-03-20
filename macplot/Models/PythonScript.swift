4aae7a613f244bf28070aac700275102389e3e78
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2021-03-19 18:28:43 -0700

    tmp file

--- macplot/Models/PythonScript.swift
+++ macplot/Models/PythonScript.swift
@@ -9,6 +9,7 @@ import Foundation
 import PythonKit
 
 class PythonScript: ObservableObject {
+    let tempURL = URL(fileURLWithPath: NSTemporaryDirectory())
     @Published var script:String = ""
     
     init() {
@@ -19,8 +20,15 @@ class PythonScript: ObservableObject {
     
     func run() {
         let sys = Python.import("sys")
-        sys.path.append("/Users/satoshi/git/mm/macplot/macplot")
-        let sample = Python.import("sample")
-        sample.main("/Users/satoshi/git/mm/macplot/macplot/plot2.png")
+        let filename = UUID().uuidString
+        let url = tempURL.appendingPathComponent("\(filename).py")
+        do {
+            try script.write(to: url, atomically: true, encoding: .utf8)
+            sys.path.append(tempURL.path)
+            let sample = Python.import(filename)
+            sample.main("/Users/satoshi/git/mm/macplot/macplot/plot2.png")
+        } catch {
+            print("error saving file")
+        }
     }
 }
