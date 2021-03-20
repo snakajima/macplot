8f04d7278fbc9805b229a49d787e9d2cfb1f134c
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2021-03-19 21:26:20 -0700

    clear

--- macplot/Models/PythonScript.swift
+++ macplot/Models/PythonScript.swift
@@ -12,22 +12,36 @@ import AppKit
 class PythonScript: ObservableObject {
     let tempURL = URL(fileURLWithPath: NSTemporaryDirectory())
     let imageURL: URL
-    let sys = Python.import("sys")
+    let clear: PythonObject?
     @Published var script: String = ""
     @Published var errorMsg: String? = nil
     @Published var image: NSImage? = nil
     
     init() {
+        let sys = Python.import("sys")
         imageURL = tempURL.appendingPathComponent("plot.png")
         if let url = Bundle.main.url(forResource: "sample", withExtension: "py") {
             script = (try? String(contentsOf: url, encoding: .utf8)) ?? ""
         }
+        if let url = Bundle.main.url(forResource: "clear", withExtension: "py") {
+            var components = url.pathComponents
+            components.removeLast()
+            let path = components.joined(separator: "/")
+            sys.path.append(path)
+            clear = Python.import("clear")
+        } else {
+            clear = nil
+        }
     }
     
     func run() {
+        let sys = Python.import("sys")
         let filename = UUID().uuidString
         let url = tempURL.appendingPathComponent("\(filename).py")
         do {
+            if let clear = clear {
+                clear.main()
+            }
             try script.write(to: url, atomically: true, encoding: .utf8)
             sys.path.append(tempURL.path)
             let sample = try Python.attemptImport(filename)
--- macplot/Scripts/clear.py
+++ macplot/Scripts/clear.py
@@ -3,5 +3,5 @@ from matplotlib import pyplot as plt
 import numpy as np
 import math #needed for definition of pi
 
-def main(path):
+def main():
     plt.clf()
