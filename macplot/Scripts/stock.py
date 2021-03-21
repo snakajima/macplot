a69710cbc7f0949e7766cd1977dc7ae1308b6852
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2021-03-21 13:34:14 -0700

    today

--- macplot/Scripts/stock.py
+++ macplot/Scripts/stock.py
@@ -4,26 +4,26 @@ import numpy as np
 import math #needed for definition of pi
 import csv
 import urllib2
+import datetime
 
 def main():
     symbol = "TSLA"
-    start = 18342 # days from 12/31/1969
+    today = (datetime.datetime.utcnow() - datetime.datetime(1970,1,1)).days
     length = 365
     params = {
-        "period1": str(start  * 24 * 60 * 60),
-        "period2": str((start + length) * 24 * 60 * 60),
+        "period1": str((today - length)  * 24 * 60 * 60),
+        "period2": str(today * 24 * 60 * 60),
         "interval": "1wk",
         "includeAdjustedClose": "true"
     }
     query = "&".join(map(lambda key: "=".join([key, params[key]]), params))
     url = "https://query1.finance.yahoo.com/v7/finance/download/"
     response = urllib2.urlopen(url + symbol + "?" + query)
-    cr = csv.DictReader(response)
-    rows = map(lambda x: x, cr)
+    rows = map(lambda x: x, csv.DictReader(response))
     
     closes = map(lambda x: x["Close"], rows)
     indeces = np.arange(len(closes))
-    ticks = filter(lambda x: x % 5 == 0, indeces)
+    ticks = filter(lambda x: x % (len(closes) / 10) == 0, indeces)
     plt.xticks(ticks, map(lambda x: rows[x]["Date"], ticks), rotation=30, ha='right')
     plt.plot(indeces, closes)
     plt.title("Historical Stock Price: " + symbol)
