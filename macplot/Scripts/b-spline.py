3bce98940548c468f3b9831461b74ef1485d5a23
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2021-03-21 20:19:25 -0700

    cubit B-Spline

--- macplot/Scripts/b-spline.py
+++ macplot/Scripts/b-spline.py
@@ -6,17 +6,27 @@ import matplotlib.pyplot as plt
 
 def main():
     ctr =np.array( [(3 , 1), (2.5, 4), (0, 1), (-2.5, 4),
-                    (-3, 0), (-2.5, -4), (0, -1), (2.5, -4), (3, -1)])
-
+                    (-3, 0), (-2.5, -4), (0, -1), (2.5, -4), (3, -1),])
     x=ctr[:,0]
     y=ctr[:,1]
 
-    tck,u = interpolate.splprep([x,y],k=3,s=0)
-    u=np.linspace(0,1,num=200,endpoint=True)
-    out = interpolate.splev(u,tck)
+    # uncomment both lines for a closed curve
+    #x=np.append(x,[x[0]])
+    #y=np.append(y,[y[0]])
 
-    plt.figure()
-    plt.plot(x, y, 'ro', out[0], out[1], 'b')
-    plt.legend(['Points', 'Interpolated B-spline', 'True'],loc='best')
+    l=len(x)
+
+    t=np.linspace(0,1,l-2,endpoint=True)
+    t=np.append([0,0,0],t)
+    t=np.append(t,[1,1,1])
+
+    tck=[t,[x,y],3]
+    u3=np.linspace(0,1,(max(l*2,70)),endpoint=True)
+    out = interpolate.splev(u3,tck)
+
+    plt.plot(x,y,'k--',label='Control polygon',marker='o',markerfacecolor='red')
+    #plt.plot(x,y,'ro',label='Control points only')
+    plt.plot(out[0],out[1],'b',linewidth=2.0,label='B-spline curve')
+    plt.legend(loc='best')
     plt.axis([min(x)-1, max(x)+1, min(y)-1, max(y)+1])
-    plt.title('B-Spline interpolation')
+    plt.title('Cubic B-spline curve evaluation')
