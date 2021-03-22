799d57d518c4aea07fb0c6445569cc40a83de398
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2021-03-21 18:35:04 -0700

    simplified

--- macplot/Scripts/poisson.py
+++ macplot/Scripts/poisson.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 from matplotlib import pyplot as plt
 import numpy as np
-from scipy.stats import poisson # definition of poisson
-import math #needed for definition of factorial
-# poisson distribution
+import math
+
 def main():
     x = np.arange(20)
     mu_list = [1, 3, 5, 10]
-    for idx, mu in enumerate(mu_list):
-        # plt.plot(x, poisson.pmf(k=x, mu=mu), 'o-', label='mu={}'.format(mu))
+    for mu in mu_list:
         y = (mu**x * np.e**(-mu))/np.vectorize(np.math.factorial)(x)
         plt.plot(x, y, 'o-', label='mu={}'.format(mu))
+
     plt.legend()
     plt.xlabel("Num. of calls wihtin a hour")
-    plt.ylabel("probability")
+    plt.ylabel("Probability")
