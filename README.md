d0a93c3e4b2c7376a5ae489b2ba1a73e5746fd94
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2021-03-22 20:47:32 -0700

    updated README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # macplot
-Python-based graph generator for MacOS
+Python-based graph generator for MacOS (11.0 or later)
+
+
--- macplot/Scripts/clear.py
+++ macplot/Scripts/clear.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+import matplotlib
 from matplotlib import pyplot as plt
 
 def main():
+    # matplotlib.use('Agg')
     plt.clf()
     plt.figure(figsize=(16, 9))
