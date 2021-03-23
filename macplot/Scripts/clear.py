d7827db9489cab482700dd9d9ee439ce7b3f8639
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2021-03-22 21:00:06 -0700

    fixed focus issue

--- macplot/Scripts/clear.py
+++ macplot/Scripts/clear.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import matplotlib
+matplotlib.use('Agg') # do not use any windows to render
 from matplotlib import pyplot as plt
 
 def main():
-    # matplotlib.use('Agg')
     plt.clf()
     plt.figure(figsize=(16, 9))
